f526e7c7359e128056665c02c21c340cdb3984d3
markusmo <markus.mo@gmx.net>
2014-01-24 15:51:46 +0100

    Generator working :-D

--- RiNaCluchi/Assets/RandomAircraftGenerator.cs
+++ RiNaCluchi/Assets/RandomAircraftGenerator.cs
@@ -1,11 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 namespace AssemblyCSharp
 {
 		public class RandomAircraftGenerator : RandomGenerator<Aircraft>
 		{
+				private List<KeyValuePair<Type,double>> elements;
+
 				public RandomAircraftGenerator ()
 				{
+						this.elements = new List<KeyValuePair<Type,double>> ();
+				}
+
+				public override void AddElement (Type element, double probability)
+				{
+						this.elements.Add (new KeyValuePair<Type, double> (element, probability));
+				}
+
+				public override Aircraft GenerateElement ()
+				{
+						Random r = new Random ();
+						double dice = r.NextDouble ();
+
+						double cumulative = 0.0;
+						for (int i = 0; i < elements.Count; i++) {
+								cumulative += elements [i].Value;
+								if (dice < cumulative) {
+										return (Aircraft) Activator.CreateInstance (elements [i].Key);
+								}
+						}
 				}
 		}
 }
