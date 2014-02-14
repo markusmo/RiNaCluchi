3bb75c65714afae547086501dab48748060e6024
Markus Mohanty <markus.mohanty@gmail.com>
2014-02-14 14:17:06 +0100

    got rid of editor

--- RiNaCluchi/Assets/SpawnController.cs
+++ RiNaCluchi/Assets/SpawnController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEditor;
+
+//using UnityEditor;
 using System.Collections.Generic;
 
 namespace AssemblyCSharp
@@ -67,10 +68,9 @@ namespace AssemblyCSharp
 				public GameObject SpawnAircraft (int runway, Vector3 position, Quaternion rotation, Aircraft aircraft)
 				{
 						//fbx has to be located in "Resources" folder in project view
-						//http://forum.unity3d.com/threads/136130-Load-FBX-into-prefab
+						//https://docs.unity3d.com/Documentation/ScriptReference/Resources.LoadAssetAtPath.html
 						//http://docs.unity3d.com/Documentation/ScriptReference/GameObject.AddComponent.html
-						GameObject spawn = AssetDatabase.LoadAssetAtPath ("Assets/Resources/" + "Cessna172.fbx", typeof(GameObject)) as GameObject;
-						PrefabUtility.CreatePrefab ("Assets/Resource/" + "Cessna172" + ".prefab", spawn);
+						GameObject spawn = Resources.LoadAssetAtPath ("Assets/Resources/" + "Cessna172.fbx", typeof(GameObject)) as GameObject;
 						spawn = GameObject.Instantiate (spawn, position, rotation) as GameObject;
 						spawn.transform.localScale = new Vector3 (6, 6, 6);
 						AirCraftController ac = (AirCraftController)spawn.AddComponent ("AirCraftController");
